db15dacb7c197500a072439bea18a3ff07e7a960
kbseydu <konradbojczuk@wp.pl>
2023-01-16 19:44:51 +0100

    Commit wszystkiego po skopiowaniu

---
+++ src/main/java/operationsIO/FileUtil.java
@@ -0,0 +1,24 @@
+package operationsIO;
+
+import java.io.*;
+
+public class FileUtil {
+
+    public String readFile(String path) throws IOException {
+        var fileReader = new FileReader(path);
+        var bufferedReader = new BufferedReader(fileReader);
+        var stringBuilder = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        return stringBuilder.toString();
+    }
+
+    public void writeFile(String path, String content) throws IOException {
+        var fileWriter = new FileWriter(path);
+        var bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(content);
+        bufferedWriter.flush();
+    }
+}
